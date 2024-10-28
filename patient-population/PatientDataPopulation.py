fd0a1ef967de78a3c4820ca4e7e5e1ccc807086b
richa13virgo <38678536+richa13virgo@users.noreply.github.com>
2024-10-27 22:35:23 -0400

    Added the code to patient population

--- patient-population/PatientDataPopulation.py
+++ patient-population/PatientDataPopulation.py
@@ -1,1 +1,27 @@
-Worked on the patient population information
+import csv
+from faker import Faker
+
+fake = Faker()
+
+races = ["White", "Black or African American", "Asian","American Indian", "Unknown","Not Reported"]
+
+genders = ["Male", "Female"]
+
+statuses = ["Active", "Inactive"]
+with open('patient.csv','w', newline='') as csvFile:
+    fieldNames = ['patientId', 'firstName','lastName','dateOfBirth', 'race','gender','registerationDate','registerationStatus','createdDate']
+    writer = csv.DictWriter(csvFile,fieldnames=fieldNames)
+
+    writer.writeheader()
+    for _ in range(1000):
+        writer.writerow({
+            'patientId': fake.unique.random_int(1,10000),
+            'firstName': fake.first_name(),
+            'lastName': fake.last_name(),
+            'dateOfBirth': fake.date_of_birth(),
+            'race': fake.random_choices(races),
+            'gender': fake.random_choices(genders),
+            'registerationDate': fake.date(),
+            'registerationStatus': fake.random_choices(statuses),
+            'createdDate': fake.date()
+        })
